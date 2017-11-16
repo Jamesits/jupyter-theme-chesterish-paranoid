20cd258b5593506321f130d87420096ccf1a3205
James Swineson <github@public.swineson.me>
2017-11-16 09:29:28 +0800

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,12 @@
+MathJax.Hub.Config({
+    "HTML-CSS": {
+        /*preferredFont: "TeX",*/
+        /*availableFonts: ["TeX", "STIX"],*/
+        styles: {
+            scale: 100,
+            ".MathJax_Display": {
+                "font-size": "100%",
+            }
+        }
+    }
+});
